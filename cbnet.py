db3fbc74e49e59f5855c6359e72367a3b294e260
Gustavo Maldonado <101005722+Gustavolpp@users.noreply.github.com>
2023-08-06 14:47:07 -0300

    Update cbnet.py
    Correção da chamada de duas rotas.

--- cbnet.py
+++ cbnet.py
@@ -2,26 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
     return render_template('index.html')
 
-app.route("/cadastro/usuario")
-def index():
+@app.route("/cadastro/usuario")
+def cadUsuario():
     return render_template('cadastro_usuario.html')
 
-app.route("/cadastro/categoria")
-def index():
+@app.route("/cadastro/categoria")
+def cadCategoria():
     return render_template('cadastro_categoria.html')
 
-app.route("/cadastro/anuncio")
-def index():
+@app.route("/cadastro/anuncio")
+def cadAnuncio():
     return render_template('cadastro_anuncio.html')
 
-app.route("/")
-def index():
+@app.route("/anuncio")
+def anuncio():
     return render_template('anuncio.html')
 
-app.route("/")
-def index():
+@app.route("/relatorio")
+def relatorio():
     return render_template('relatorio.html')
